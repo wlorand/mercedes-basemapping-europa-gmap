332e9b97dc0d9fd5eae4c85a767223677ed75e03
Karthk Ashok Arjunagi <karthik_ashok.arjunagi@daimler.com>
2017-09-13 14:57:58 -0700

    initial gmaps proto for md-rd

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -4,17 +4,40 @@
 
 function initMap(){
 
-	console.log('here will still still be the gmap');
+	// 1- set some key mapping vars (map options, map center, map styles)
+	const mapDiv = document.getElementById('map-container');
+	const germanyCenter = new google.maps.LatLng(51.165691, 10.451526); // centroid point de
+	
+	const tanEarthStyles = [
+		{
+		stylers: [
+				{ hue: '#DCBE98' }, 
+				{ invert_lightness: false},
+            	{ saturation: -20 }
+			]
+		} 
+	];
 
-	const name = 'Will';
-	console.log(name);
+	const mapOptions = {
+    	zoom: 6,
+      	center: germanyCenter,
+      	mapTypeId: google.maps.MapTypeId.TERRAIN, // ROADMAP  / TERRAIN / SATELLITE 
+    	// controls
+    	disableDefaultUI: true,
+	  	panControl: false,
+	  	zoomControl: true,
+	  	zoomControlOptions: {
+	    	style: google.maps.ZoomControlStyle.SMALL,
+	     	position: google.maps.ControlPosition.LEFT_BOTTOM
+	  	},
+    	// styles
+    	styles: tanEarthStyles
+    };
 
-
-
+    // 2- create the Map object 
+    const map = new google.maps.Map(mapDiv, mapOptions);
 
 }
-
-
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', initMap);
